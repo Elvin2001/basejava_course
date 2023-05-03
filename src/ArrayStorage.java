e4946e6b8dbd6536b847d36a4fe5837851699776
Elvin Abaev <elvinabaev@gmail.com>
2023-05-03 14:20:36 +0300

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,25 +6,16 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
 
-    // Добавляем поле о максимальном количестве элементов в хранилище
     protected static final int STORAGE_LIMIT = 10000;
-
-    // Добавляем поле о размере хранилища
-    protected int size;
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
-    public void iterateOfStorage(Resume[] storage) {
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] != null) {
-                System.out.println(storage[i]);
-            }
-        }
-    }
+    protected int size;
     void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
+
     void save(Resume r) {
-        if (size >= storage.length) {
+        if (size >= STORAGE_LIMIT) {
             System.out.println("Хранилище переполнено");
         } else {
             int index = findIndex(r.getUuid());
@@ -36,15 +27,16 @@ public class ArrayStorage {
             }
         }
     }
+
     Resume get(String uuid) {
         int index = findIndex(uuid);
         if (index < 0) {
             System.out.println("Такого объекта не было найдено");
-        } else {
+            return null;
+        }
             return storage[index];
         }
-        return null;
-    }
+
     void delete(String uuid) {
         int index = findIndex(uuid);
         if (index < 0) {
@@ -62,10 +54,11 @@ public class ArrayStorage {
     Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
+
     int size() {
         return size;
     }
-    private Integer findIndex(String uuid) {
+    private int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
